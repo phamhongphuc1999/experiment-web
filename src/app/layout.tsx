dccd30167f0407e1fb8febc900ddabd009f3f189
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2026-03-01 11:03:26 +0700

    fix: google analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Fira_Code } from 'next/font/google';
+import Script from 'next/script';
 import { ReactNode } from 'react';
 import LayoutWrapper from 'src/components/LayoutWrapper';
 import { MetadataHead } from 'src/components/MetadataHead';
@@ -23,6 +24,19 @@ export default function RootLayout({ children }: Props) {
     <html lang="en">
       <MetadataHead />
       <body data-theme="dark" className={`${firaCode.className} antialiased`}>
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5DTLDHPJ');
+  `,
+          }}
+        />
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5DTLDHPJ"
