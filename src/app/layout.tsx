c0685b53851ad5d31e7f39c47150612bc663e07c
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2026-03-09 13:06:50 +0700

    fix: gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,17 +29,17 @@ export default function RootLayout({ children }: Props) {
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5DTLDHPJ');
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W3M57J7H');
   `,
           }}
         />
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5DTLDHPJ"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W3M57J7H"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
